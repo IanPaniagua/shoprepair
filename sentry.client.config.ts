d9d20857b0fd144969b6d1313009000334d80a94
IanPaniagua <paniagua.ian.de@gmail.com>
2025-03-15 18:27:52 +0100

    installing sentry to error monitoring

---
+++ sentry.client.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the client.
+// The config you add here will be used whenever a users loads a page in their browser.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://52f424b53f8597f693a05298da99194e@o4508982869950464.ingest.de.sentry.io/4508982874210384",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
