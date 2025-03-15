a1d1917d4da194ef10aff18094ef63049537bd20
IanPaniagua <paniagua.ian.de@gmail.com>
2025-03-15 13:19:15 +0100

    adding appear animation to template page

--- src/app/(rs)/layout.tsx
+++ src/app/(rs)/layout.tsx
@@ -7,7 +7,7 @@ export default async function RSLayout({
     return (
         <div className="mx-auto w-full max-w-7xl">
             <Header />
-            <div className="px-4 py-2">
+            <div className=" px-4 py-2">
                 {children}
             </div>
         </div>
